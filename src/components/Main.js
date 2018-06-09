701456003d42d9b04e2893cfda68d9a7bbf8f557
Andy Tran <andy8tran@gmail.com>
2018-06-08 23:43:15 -0400

    Change content

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,76 +6,178 @@ import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 
 class Main extends React.Component {
-  render() {
+    render() {
+        let close = (
+            <div
+                className="close"
+                onClick={() => {
+                    this.props.onCloseArticle()
+                }}
+            />
+        )
 
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+        const applicationURL =
+            'https://docs.google.com/forms/d/1I73zEWzUlrjvWL4LD6kuhHg0DzZQs5MnABVgnI2gl7o/edit'
 
-    return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+        return (
+            <div
+                id="main"
+                style={
+                    this.props.timeout
+                        ? { display: 'flex' }
+                        : { display: 'none' }
+                }
+            >
+                <article
+                    id="intro"
+                    className={`${
+                        this.props.article === 'intro' ? 'active' : ''
+                    } ${this.props.articleTimeout ? 'timeout' : ''}`}
+                    style={{ display: 'none' }}
+                >
+                    <h2 className="major">Intro</h2>
+                    <span className="image main">
+                        <img src={pic01} alt="" />
+                    </span>
+                    <p>
+                        Aenean ornare velit lacus, ac varius enim ullamcorper
+                        eu. Proin aliquam facilisis ante interdum congue.
+                        Integer mollis, nisl amet convallis, porttitor magna
+                        ullamcorper, amet egestas mauris. Ut magna finibus nisi
+                        nec lacinia. Nam maximus erat id euismod egestas. By the
+                        way, check out my <a href="#work">awesome work</a>.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Duis dapibus rutrum facilisis. Class aptent taciti
+                        sociosqu ad litora torquent per conubia nostra, per
+                        inceptos himenaeos. Etiam tristique libero eu nibh
+                        porttitor fermentum. Nullam venenatis erat id vehicula
+                        viverra. Nunc ultrices eros ut ultricies condimentum.
+                        Mauris risus lacus, blandit sit amet venenatis non,
+                        bibendum vitae dolor. Nunc lorem mauris, fringilla in
+                        aliquam at, euismod in lectus. Pellentesque habitant
+                        morbi tristique senectus et netus et malesuada fames ac
+                        turpis egestas. In non lorem sit amet elit placerat
+                        maximus. Pellentesque aliquam maximus risus, vel sed
+                        vehicula.
+                    </p>
+                    {close}
+                </article>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
-          {close}
-        </article>
+                <article
+                    id="midya"
+                    className={`${
+                        this.props.article === 'midya' ? 'active' : ''
+                    } ${this.props.articleTimeout ? 'timeout' : ''}`}
+                    style={{ display: 'none' }}
+                >
+                    <h2 className="major">Kamalayan Midya Project 2018</h2>
+                    <p>
+                        In collaboration with{' '}
+                        <a href="https://artreach.org/" target="_blank">
+                            ArtReach
+                        </a>{' '}
+                        in Toronto, Kamalyan is putting together a summer media
+                        project for Filipino youth in the Toronto area. Our
+                        goals are to get youth involved with media production;
+                        to develop their technical, program development, and
+                        speaking skills for radio and TV; and to inspire them to
+                        create social change. We strive to connect Philippine
+                        youth to their culture and to issues in both Canada and
+                        the Philippines.
+                    </p>
+                    <p>
+                        If you're interested, you can view the application
+                        <a href={applicationURL}> here</a>. The deadline for
+                        applications is June 24, 2018. We look forward to
+                        hearing from you!
+                    </p>
+                    {close}
+                </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-          {close}
-        </article>
+                <article
+                    id="about"
+                    className={`${
+                        this.props.article === 'about' ? 'active' : ''
+                    } ${this.props.articleTimeout ? 'timeout' : ''}`}
+                    style={{ display: 'none' }}
+                >
+                    <h2 className="major">About</h2>
+                    <p>
+                        Kamalayan means “awareness” or “consciousness” in
+                        Filipino. We aim to uplift and inform young people so
+                        that they may move forward responsibly, conscious of
+                        their power and their crucial role in creating change.
+                        Kamalayan’s regular workshops and events take a critical
+                        look at Philippine history, culture, and current events
+                        to encourage, dialogue, awareness and consciousness as a
+                        foundation for youth empowerment.
+                    </p>
+                    <p>We are based in Toronto, Canada.</p>
+                    {close}
+                </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
-          {close}
-        </article>
-
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+                <article
+                    id="contact"
+                    className={`${
+                        this.props.article === 'contact' ? 'active' : ''
+                    } ${this.props.articleTimeout ? 'timeout' : ''}`}
+                    style={{ display: 'none' }}
+                >
+                    <h2 className="major">Contact</h2>
+                    <p>
+                        If you'd like to get in touch, you can fill out the form
+                        below, or shoot us an email at{' '}
+                        <a href="mailto:info@kamalayan.ca">info@kamalayan.ca</a>.
+                        You can also find us on Facebook{' '}
+                        <a href="https://www.facebook.com/kamalayankonsciousness/">
+                            {' '}
+                            here
+                        </a>.
+                    </p>
+                    <form
+                        method="POST"
+                        action="https://formspree.io/info@kamalayan.ca"
+                    >
+                        <div className="field half first">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" id="name" />
+                        </div>
+                        <div className="field half">
+                            <label htmlFor="email">Email</label>
+                            <input type="text" name="email" id="email" />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" rows="4" />
+                        </div>
+                        <ul className="actions">
+                            <li>
+                                <input
+                                    type="submit"
+                                    value="Send Message"
+                                    className="special"
+                                />
+                            </li>
+                            <li>
+                                <input type="reset" value="Reset" />
+                            </li>
+                        </ul>
+                    </form>
+                    {close}
+                </article>
             </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
-          <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
-          </ul>
-          {close}
-        </article>
-
-      </div>
-    )
-  }
+        )
+    }
 }
 
 Main.propTypes = {
-  route: React.PropTypes.object,
-  article: React.PropTypes.string,
-  articleTimeout: React.PropTypes.bool,
-  onCloseArticle: React.PropTypes.func,
-  timeout: React.PropTypes.bool
+    route: React.PropTypes.object,
+    article: React.PropTypes.string,
+    articleTimeout: React.PropTypes.bool,
+    onCloseArticle: React.PropTypes.func,
+    timeout: React.PropTypes.bool,
 }
 
 export default Main
