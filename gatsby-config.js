cbf6950534726ee74db1ff8ca737453c36faba5e
Andy Tran <andy8tran@gmail.com>
2018-06-09 12:34:40 -0400

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,60 @@
 module.exports = {
-  siteMetadata: {
-    title: "Gatsby Starter - Dimension by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Dimension by HTML5 UP"
-  },
-  pathPrefix: '/',
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/posts`,
-        name: "posts",
-      },
+    siteMetadata: {
+        title: 'Kamalayan',
+        author: 'Kamalayan',
+        description: 'A journey into responsible Filipino-ness',
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    pathPrefix: '/',
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 630,
+                path: `${__dirname}/src/posts`,
+                name: 'posts',
             },
-          },
-          "gatsby-remark-copy-linked-files",
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
-  ],
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 630,
+                        },
+                    },
+                    'gatsby-remark-copy-linked-files',
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: `${__dirname}/src/images/ka.png`,
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: false,
+                },
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+    ],
 }
