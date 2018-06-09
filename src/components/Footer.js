a4c360ed56c0b80f879a95189741b0f0a2a64f6c
Andy Tran <andy8tran@gmail.com>
2018-06-09 12:34:59 -0400

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Footer = props => (
-  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
-    <p className="copyright">
-      &copy; Kamalayan. Image: <a href="hannah.ph">Hannah Reyes</a>. Built with:{' '}
-      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
-    </p>
-  </footer>
+    <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+        <p className="copyright">
+            &copy; Kamalayan. Image:{' '}
+            <a href="http://hannah.ph">Hannah Reyes Morales</a>.
+        </p>
+    </footer>
 )
 
 Footer.propTypes = {
-  timeout: React.PropTypes.bool,
+    timeout: React.PropTypes.bool,
 }
 
 export default Footer
