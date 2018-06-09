e00378e49c4c70cfc184ed52aabe9dc90cc53abf
Andy Tran <andy8tran@gmail.com>
2018-06-08 23:43:27 -0400

    Add images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      &copy; Kamalayan. Image: <a href="hannah.ph">Hannah Reyes</a>. Built with:{' '}
+      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
+    </p>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: React.PropTypes.bool
+  timeout: React.PropTypes.bool,
 }
 
 export default Footer
