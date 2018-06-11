8f97fd8b222f86faff40be10601366ae0c3e3ffb
Andy Tran <andy8tran@gmail.com>
2018-06-11 09:21:39 -0400

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ErrorPage = () => {
+    return (
+        <div
+            style={{
+                width: '95vw',
+            }}
+        >
+            Sorry, it looks like this page doesn't exist!
+        </div>
+    )
+}
+
+export default ErrorPage
